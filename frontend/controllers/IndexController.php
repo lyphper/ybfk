8fb37d9e6f15e7fad68c33f6bf88593cc291adff
ly <liyong@corp.1jiajie.com>
2016-05-04 15:24:04 +0800

    test

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -5,8 +5,12 @@
  * Date: 16-4-19
  * Time: 下午2:19
  */
+namespace  frontend\controllers;
 
-class IndexController extends  \yii\base\Controller{
+use Yii;
+use yii\web\Controller;
+
+class IndexController extends Controller{
 
 
 }
